69bb9ace40d94f11b9d78463b226c2cd2fdd23f7
Ram Karthi <karthi51216@gmail.com>
2026-04-09 20:55:50 +0530

    add create user api

--- task 1/backend/accounts/urls.py
+++ task 1/backend/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import DashboardView
+from .views import DashboardView,create_user
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('create_user/', create_user, name='create_user'),  
 ]
--- task 1/backend/accounts/views.py
+++ task 1/backend/accounts/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 class DashboardView(APIView):
@@ -13,3 +16,10 @@ class DashboardView(APIView):
             "message": "Welcome to dashboard!",
             "user": request.user.username
         })
+    
+    @api_view(['GET'])
+    def create_user(request):
+     if not User.objects.filter(username="admin").exists():
+        User.objects.create_user(username="admin", password="admin123")
+        return Response({"message": "User created"})
+     return Response({"message": "User already exists"})
