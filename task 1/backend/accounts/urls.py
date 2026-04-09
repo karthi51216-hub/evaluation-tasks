60de0d58ee47718ecb582432521fff406897009b
Ram Karthi <karthi51216@gmail.com>
2026-04-09 21:08:01 +0530

    added create user api

--- task 1/backend/accounts/urls.py
+++ task 1/backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import DashboardView,create_user
+from .views import DashboardView, create_user
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
